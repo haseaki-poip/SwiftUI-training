307c808bfd8f66d48048df4362fee9c2ad2b2d26
Hasegawa-Akito <h.akito1999@gmail.com>
2023-01-26 20:56:33 +0900

    Initial Commit

---
+++ SwiftUI-Training/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Training
+//
+//  Created by Hasegawa Akito on 2023/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Training/SwiftUI_TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TrainingApp.swift
+//  SwiftUI-Training
+//
+//  Created by Hasegawa Akito on 2023/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
