753782ddce7873b6831ee015e7a89152b8914cd1
Hasegawa-Akito <h.akito1999@gmail.com>
2023-01-30 10:47:05 +0900

    class作成とキーボード入力

--- SwiftUI-Training/ContentView.swift
+++ SwiftUI-Training/ContentView.swift
@@ -3,47 +3,62 @@ import SwiftUI
 
 struct ContentView: View {
     @State var imgName = "snowboard"
+    @State var inputText = ""
+    @ObservedObject var okashiDatalist = OkashiData()
     
     var body: some View {
-        VStack {
-            ZStack {
-                Image(imgName)
-                    .resizable() // 画面のサイズにぴったり収まるようにする
-                    .aspectRatio(contentMode: .fit) // 崩れた縦横比をうまくフィットさせる
-                Color.gray
-                    .frame(width: 200, height: 100)
-                    .opacity(0.5)
-                Text(imgName)
-                    .foregroundColor(Color.white)
-            }
-            
-            HStack {
-                Button("前へ") {
-                    imgName = "snowboard"
-                }
-                .padding()
-                .background(Color.gray)
-                .foregroundColor(Color.white)
-                .padding(.trailing, 10)
-                
-                Button("次へ") {
-                    imgName = "mycar"
-                }
-                .padding()
-                .background(Color.gray)
-                .foregroundColor(Color.white)
-                .padding(.leading, 10)
-                
-            }
-            NavigationView {
-                NavigationLink("ページ遷移", destination: Text("2ページ目")
-                                .navigationTitle("ページ2")
-                                
+        NavigationView {
+            VStack {
+                TextField("テキストを入力", text: $inputText, onCommit: {
+                    okashiDatalist.serchOkashi(keyword: inputText)
+                    }
                 )
+                ZStack {
+                    Image(imgName)
+                        .resizable() // 画面のサイズにぴったり収まるようにする
+                        .aspectRatio(contentMode: .fit) // 崩れた縦横比をうまくフィットさせる
+                    Color.gray
+                        .frame(width: 200, height: 100)
+                        .opacity(0.5)
+                    Text(imgName)
+                        .foregroundColor(Color.white)
+                }
+                
+                HStack {
+                    Button("前へ") {
+                        imgName = "snowboard"
+                    }
+                    .padding()
+                    .background(Color.gray)
+                    .foregroundColor(Color.white)
+                    .padding(.trailing, 10)
+                    
+                    Button("次へ") {
+                        imgName = "mycar"
+                    }
+                    .padding()
+                    .background(Color.gray)
+                    .foregroundColor(Color.white)
+                    .padding(.leading, 10)
+                    
+                }
+                NavigationView {
+                    NavigationLink("1.1ページ目遷移", destination: Text("1.1ページ目")
+                                    .navigationTitle("ページ1.1")
+                                    
+                    )
+                }
+                .frame(height: 200)
+                // このNavigationViewの範囲内のところのみが遷移する。
+                
             }
-            .frame(height: 200)
-            // このNavigationViewの範囲内のところのみが遷移する。
-            
+            .toolbar {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    Button("2ページ目遷移") {
+                       
+                    }
+                }
+            }
         }
         
     }
---
+++ SwiftUI-Training/OkashiData.swift
@@ -0,0 +1,13 @@
+//
+//  OkashiData.swift
+//  SwiftUI-Training
+//
+//  Created by Hasegawa Akito on 2023/01/30.
+//
+
+import Foundation
+class OkashiData: ObservableObject {
+    func serchOkashi(keyword: String){
+        print(keyword)
+    }
+}
