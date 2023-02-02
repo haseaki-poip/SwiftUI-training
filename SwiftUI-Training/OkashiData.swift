a75cd3b53ede724c8d4f83921604a034cf7f9991
Hasegawa-Akito <h.akito1999@gmail.com>
2023-02-02 23:34:46 +0900

    画像サイズ変更

--- SwiftUI-Training/OkashiData.swift
+++ SwiftUI-Training/OkashiData.swift
@@ -32,9 +32,12 @@ class OkashiData: ObservableObject {
     
     @Published var okashiList: [OkashiItem] = []
     
-    func serchOkashi(keyword: String){
+    init() {
+        self.serchOkashi(keyword: "カレー味")
         
-        print(keyword)
+    }
+    
+    func serchOkashi(keyword: String){
         
         // キーワードをエンコード
         guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
@@ -42,11 +45,10 @@ class OkashiData: ObservableObject {
         }
         
         // リクエストURLを組み立て
-        guard let req_url = URL(string: "https://www.sysbird.jp/webapi/?apikey=guest&format=json&keyword=\(keyword_encode)&max=10&order=r") else {
+        guard let req_url = URL(string: "https://www.sysbird.jp/webapi/?apikey=guest&format=json&keyword=\(keyword_encode)&max=15&order=r") else {
             return
         }
         
-        print(req_url)
         
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         // 画面更新やPublishedを更新する際メインスレッドで行う必要がある
@@ -69,14 +71,13 @@ class OkashiData: ObservableObject {
            }
            
            if response.statusCode == 200 {
-               // 結果：通信結果のJSONをStringで得る。
-               print(String(data: data, encoding: .utf8)!)
+//               // 結果：通信結果のJSONをStringで得る。
+//               print(String(data: data, encoding: .utf8)!)
                
                // パターン2:JSON形式を構造体(Struct）に変換して使用する
                do {
                    
                    let json = try JSONDecoder().decode(ResultJson.self, from: data)
-                   print(json)
                    
                    if let items = json.item { // itemsに代入しつつ、それがnilかどうか。nilでなければその{}の処理を行え、その中でitemsを使える
                        self.okashiList.removeAll()
@@ -99,7 +100,83 @@ class OkashiData: ObservableObject {
                        self.okashiList.removeAll()
                    }
                    
-                   print(self.okashiList)
+                   
+               } catch let error {
+                   print(":エラー:\(error)") // JSONの値がIDがIntなのに、StructでIDをStringと宣言している時などエラーになる。
+               }
+               
+               
+           } else {
+               print("statusCode:\(response.statusCode)")
+           }
+        })
+        
+        task.resume()
+    }
+    
+    // お菓子をlistに追加していく
+    func addOkashiForList(){
+        let keyword = "カレー味"
+        
+        // キーワードをエンコード
+        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return
+        }
+        
+        // リクエストURLを組み立て
+        guard let req_url = URL(string: "https://www.sysbird.jp/webapi/?apikey=guest&format=json&keyword=\(keyword_encode)&max=10&order=r") else {
+            return
+        }
+        
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        // 画面更新やPublishedを更新する際メインスレッドで行う必要がある
+        // OperationQueue.mainによって可能
+        // OperationQueue.mainを設定しないと外部でPublishedを変更できないと警告が出る
+
+        let task = session.dataTask(with: req_url, completionHandler: { data , response, error in
+           session.finishTasksAndInvalidate()
+            
+           if let error = error {
+               print(error.localizedDescription)
+               print("通信が失敗しました")
+               return
+           }
+           
+           guard let data = data,
+                 let response = response as? HTTPURLResponse else {
+               print("データもしくはレスポンスがnilの状態です")
+               return
+           }
+           
+           if response.statusCode == 200 {
+//               // 結果：通信結果のJSONをStringで得る。
+//               print(String(data: data, encoding: .utf8)!)
+               
+               // パターン2:JSON形式を構造体(Struct）に変換して使用する
+               do {
+                   
+                   let json = try JSONDecoder().decode(ResultJson.self, from: data)
+                   
+                   if let items = json.item { // itemsに代入しつつ、それがnilかどうか。nilでなければその{}の処理を行え、その中でitemsを使える
+                       
+                       for item in items {
+                           if let name = item.name,      // 値があれば次の行を実行
+                              let link = item.url,       // 値があれば次の行を実行
+                              let imageURL = item.image, // 値があれば次の行を実行
+                              let imageData = try? Data(contentsOf: imageURL), // 値があれば次の行を実行
+                              let image = UIImage(data: imageData)?.withRenderingMode(.alwaysOriginal) {
+                               
+                               // ifにより上の要素が一つもnilでないため以下の処理を行うことができる
+                               let okashi = OkashiItem(name: name, link: link, image: image)
+                               self.okashiList.append(okashi)
+                               
+                           }
+                       }
+                   }
+                   else {
+                       self.okashiList.removeAll()
+                   }
                    
                    
                } catch let error {
@@ -114,4 +191,9 @@ class OkashiData: ObservableObject {
         
         task.resume()
     }
+    
+    func removeFirstOkashiInList() {
+        self.okashiList.removeFirst()
+    }
+    
 }
--- SwiftUI-Training/Views/SwipeView.swift
+++ SwiftUI-Training/Views/SwipeView.swift
@@ -30,57 +30,80 @@ struct CardView: View {
     @State var transation: CGSize = .zero // (x: 0, y: 0)
     @State var numbers = [0, 1, 2, 3, 4, 5]
     
+    @ObservedObject var okashiDatalist = OkashiData()
+    
+    
     var body: some View {
         
         
         GeometryReader(content: { geometry in
             
-            ForEach(numbers, id: \.self) { number in
+            ForEach(okashiDatalist.okashiList.reversed(), id: \.self.id) { okashi in
                 ZStack {
                     
                     Color.white
-                    AsyncImage(url: URL(string: "https://sysbird.jp/toriko/wp-content/blogs.dir/2/files/2019/12/10099.jpg")) { image in
-                                image
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit) // 横か縦がぴったりになるところで縦横比を保ちサイズを整える
-                                    .padding(.all, 40)
-                                    
-                            } placeholder: {
-                                // 読み込み中のViewを表示
-                                Image("snowboard")
-                                    .resizable()
-                                    .scaledToFill() // 縦横比を保ちながらframeを埋め尽くす
-                                    // padingで開ける分画像のサイズを小さくする
-                                    .frame(width: geometry.size.width - 20, height: geometry.size.height - 20)
-                            }
-    //
+                    
+                    // 外部URLを使ってImageを表示する時
+//                    AsyncImage(url: URL(string: "ここにURL")) { image in
+//                                image
+//                                    .resizable()
+//                                    .aspectRatio(contentMode: .fit) // 横か縦がぴったりになるところで縦横比を保ちサイズを整える
+//                                    .padding(.all, 40)
+//
+//                            } placeholder: {
+//                                // 読み込み中のViewを表示
+//                                Image("snowboard")
+//                                    .resizable()
+//                                    .scaledToFill() // 縦横比を保ちながらframeを埋め尽くす
+//                                    // padingで開ける分画像のサイズを小さくする
+//                                    .frame(width: geometry.size.width - 20, height: geometry.size.height - 20)
+//                            }
+                    
+                    Image(uiImage: okashi.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit) // 横か縦がぴったりになるところで縦横比を保ちサイズを整える
+                        .frame(width: 200, height: 200)
                         
                 }
                 .cornerRadius(10)
-                .padding(.all, 10)
-                .shadow(radius: 10)
-                .offset(numbers.last == number ? transation : .zero)
-                .rotationEffect(numbers.last == number ? .degrees(Double(transation.width / 15)) : .zero)
+                .padding(.all, 30)
+                .shadow(radius: 1.1)
+                .offset(okashiDatalist.okashiList.first!.id == okashi.id ? transation : .zero)
+                .rotationEffect((okashiDatalist.okashiList.first!.id == okashi.id ? .degrees(Double(transation.width / 15)) : .zero))
                 .gesture(
                     DragGesture()
                         .onChanged({ value in
                             transation = value.translation
-                            print(value.translation.width)
                         })
                         .onEnded({ value in
+            
                             if value.startLocation.x - 150 > value.location.x {
                                 transation = .init(width: -800, height: 0)
                                 // 0.3秒遅らせて配列から削除して、animationを最後まで行わせる
                                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.3, execute: {
-                                        numbers.removeLast()
+                                    okashiDatalist.removeFirstOkashiInList()
+                                    if okashiDatalist.okashiList.count == 5 {
+                                        okashiDatalist.addOkashiForList()
+                                    }
+                                    
+                                    
+                                    
+//                                        numbers.removeFirst()
                                         transation = .zero
+                                        
                                 })
                                 
                             }
                             else if value.startLocation.x + 150 < value.location.x {
                                 transation = .init(width: 800, height: 0)
                                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.3, execute: {
-                                        numbers.removeLast()
+                                    okashiDatalist.removeFirstOkashiInList()
+                                    if okashiDatalist.okashiList.count == 5 {
+                                        okashiDatalist.addOkashiForList()
+                                    }
+                                        
+                                    
+//                                        numbers.removeFirst()
                                         transation = .zero
                                 })
                             }
